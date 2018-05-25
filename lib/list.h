e571545b52d0c3fd036a1ef366299783522f611c
zhangshuai <zhangshuaiisme@gmail.com>
2018-05-25 13:29:28 +0800

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   lib/list.c
    	modified:   lib/list.h
    	new file:   lib/stdbool.h
    	modified:   sc/system_call.c
    	modified:   sc/system_call.h

---
+++ lib/list.h
@@ -0,0 +1,28 @@
+/*
+ * 双向循环链表
+ * 
+ * list_node应当放入需要的管理的new_struct内部
+ * 在管理list_node时，new_struct自然的被串联起来
+ *
+ * author:Shuai Zhang
+ * email:zhangshuaiisme@gmail.com
+ */
+
+#ifndef LIST_H
+#define LIST_H
+
+#include "stdint.h"
+
+//链表节点
+typedef struct list_node{
+	struct list_node *next, *prev;
+}list_node;
+
+/*
+ * 
+ * 用法：list_entry(&list_node,struct page,list_node)
+ * */
+#define list_entry(link, type, member) \
+	((type *)((char *)(link)-(unsigned long)(&((type *)0)->member)))
+
+#endif
---
+++ lib/stdbool.h
@@ -0,0 +1,8 @@
+#ifndef STDBOOL_H
+#define STDBOOL_H
+
+typedef enum boolean {  
+	FALSE,TRUE
+}bool;
+
+#endif
