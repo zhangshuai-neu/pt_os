9c844fb9c225e68233dd1e62f3f5a3b6fb129aea
zhangshuai <zhangshuaiisme@gmail.com>
2018-06-08 22:12:29 +0800

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   lib/test_elf/Makefile
    	modified:   lib/test_elf/test
    	modified:   lib/test_elf/test.c

--- lib/test_elf/test.c
+++ lib/test_elf/test.c
@@ -29,9 +29,38 @@ void show_elf_header(elf_header *eh_p){
 	printf("\n");
 }
 
+//显示segment header
+void show_seg_header(elf_seg_header * seg_h_array, int seg_num){
+	int i=0;
+	
+	printf("\nELF Segment header:\n");
+	for(i=0; i<seg_num; i++){
+		printf("  Segment %d\n",i);
+		
+		
+		printf("    Type:  ");
+		printf("%x\n",seg_h_array[i].seg_type);
+		switch(){
+			
+		}
+		
+		printf("    Offset:  ");
+		printf("%x\n",seg_h_array[i].seg_offset);
+		
+		printf("    Segment Filesize:  ");
+		printf("%x\n",seg_h_array[i].seg_filesz);
+		
+		printf("    Memory Filesize:  ");
+		printf("%x\n",seg_h_array[i].seg_memsz);
+		
+	}
+	printf("\n");
+}
+
 //测试example.elf文件的内容
 //格式为 elf_32
 int main(){
+	int i=0;
 	FILE * example_fp = fopen("./example.elf","rb");
 	
 	if(example_fp == NULL){
@@ -44,16 +73,22 @@ int main(){
 	fread(eh_p,sizeof(elf_header),1,example_fp);
 	show_elf_header(eh_p);
 	
-	//读取 segment header
-	fseek(example_fp,,SEEK_SET)
+	//读取 segment 头部
+	int seg_h_offset = eh_p->elf_phoff;
 	int seg_h_num = eh_p->elf_phnum;
+		
+	elf_seg_header * seg_h_array = (elf_seg_header *)malloc(seg_h_num*sizeof(elf_seg_header));
+		
+	fseek(example_fp,seg_h_offset,SEEK_SET);	
+	fread(seg_h_array,sizeof(elf_seg_header),seg_h_num,example_fp);
 	
-	elf_seg_header * seg_h_array = \
-		(elf_seg_header *)malloc(seg_h_num*sizeof(elf_seg_header));
-	
+	show_seg_header(seg_h_array,seg_h_num);
+
 	
 	//
 	
+	free(eh_p);
+	free(seg_h_array);
 	
 	fclose(example_fp);
 	
