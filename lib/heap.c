01232202ae7f3d15d187bb90b8cdc93624c81aff
ShuaiZhang <zhangshuaiisme@gmail.com>
2018-11-27 21:04:22 +0800

    some mod

--- lib/heap.c
+++ lib/heap.c
@@ -1,31 +1,71 @@
-#include "heap.h"
-
 /*
+    参考FreeRTOS heap4
     使用heap结构管理一段内存
     管理方式：
-    heap内存的开头为heap_block，用来记录内存的使用情况
-    内存从heap内存的尾部开始分配
-
-    /-----
-    /heap_block
-    /v
-    /
-    /^
-    /allocte mem
-    /------
+        管理heap_block的链表
+        每个heap_block包含heap_block结构和申请的内存空间
+        next_block_ptr的UINT32_TOP_BIT表明是不是
 
 */
 
-// heap结构初始化
-void init_heap(struct heap * hp, uint32_t base_addr, uint32_t end_addr){
-    hp->heap_base_addr = base_addr;
-    hp->heap_base_addr = end_addr;
-    hp->heap_block_count = 0;
+#include "heap.h"
+
+// heap_block初始化
+// flag 指明是free还是use
+void heap_block_init(struct heap_block* hp, uint32_t size, uint8_t flag){
+    hp->next_block_ptr = NULL;
+    hp->block_size = size;
+    if(flag==FREE_BLOCK){
+        SET_FREE(hp->next_block_ptr);
+    }
+    if(flag==USE_BLOCK){
+        SET_USE(hp->next_block_ptr);
+    }
 }
 
-//从heap中申请一段内存
+// 从小到大的顺序插入
+// in_block的位置一定在 head和end之间
+void heap_block_insert(struct heap_block* head, struct heap_block * in_block){
+    struct heap_block* temp = head;
+    struct heap_block* pre = temp;
+    while((uint32_t)temp < (uint32_t)in_block){
+        pre = temp;
+        temp = temp->next_block_ptr;
+    }
+    pre->next_block_ptr = in_block;
+    in_block->next_block_ptr = temp;
+}
+
+
+// heap初始化
+void heap_init(struct heap * hp, uint32_t heap_base_addr, uint32_t heap_size){
+
+    hp->heap_size = heap_size;
+
+    // heap_block 头尾结点初始化 
+    hp->hb_head = (struct heap_block*)heap_base_addr;
+    
+    uint32_t end_addr = heap_base_addr + heap_size -sizeof(struct heap_block);
+    hp->hb_end = (struct heap_block *)end_addr;
+
+    heap_block_init(hp->hb_head,0,FREE_BLOCK);
+    heap_block_init(hp->hb_end,0,USE_BLOCK);
+    hp->hb_head->next_block_ptr = hp->hb_end;
+
+    // 初始化 free_block, 并将其插入链表
+    struct heap_block * free_block = (hp->hb_head + 1);
+    heap_block_init(free_block, heap_size - 3*sizeof(struct heap_block));
+    heap_block_insert(hp->hb_head, free_block);
+}
+
+/**
+ * 从heap中申请一段内存
+ * 找到大小最合适的free_block，
+ * 如果free_block比较大，则将其分割
+
+ **/ 
 void * heap_malloc(struct heap *hp, uint32_t size){
-    uint32_t index  = hp->heap_block_count+1;
+    
 }
 
 //归还heap中的内存
--- lib/heap.h
+++ lib/heap.h
@@ -8,15 +8,25 @@
 
 #include "std_type_define.h"
 
-struct heap{
-    uint32_t heap_base_addr;         //heap空间的基地址
-    uint32_t heap_end_addr;
-    uint32_t heap_block_count;
-};
-
 struct heap_block{
-    uint32_t block_base_addr;
+    struct heap_block * next_block_ptr;
     uint32_t block_size;
 };
+
+struct heap{
+    // heap空间的基地址，同时也是heap_block头的位置
+    struct heap_block* hb_head;
+    struct heap_block* hb_end;
+    // heap的大小   
+    uint32_t heap_size;
+};
+
+#define UINT32_TOP_BIT ((uint32_t)1<<31)
+
+#define SET_FREE(BLOCK_ADDR) ((uint32_t)BLOCK_ADDR | UINT32_TOP_BIT)
+#define SET_USE(BLOCK_ADDR)  ((uint32_t)BLOCK_ADDR & (!UINT32_TOP_BIT)
+
+#define USE_BLOCK  ((uint8_t)0)
+#define FREE_BLOCK ((uint8_t)1)
 
 #endif
