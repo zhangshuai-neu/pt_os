d26ed2b7e6902c16b8a36b33e5e14d7b7b184dcc
ShuaiZhang <zhangshuaiisme@gmail.com>
2018-12-10 15:54:29 +0800

    some code

--- lib/rb_tree.c
+++ lib/rb_tree.c
@@ -9,11 +9,15 @@ void rb_tree_init(struct rb_tree * tree){
 // 从rb_tree中插入
 void rb_tree_insert(struct rb_tree * tree, struct rb_tree * in_node){
 
-    
 }
 
 // 从rb_tree中删除节点
 void rb_tree_remove(struct rb_tree * tree, struct rb_tree * out_node){
 
+}
+
+
+// 从rb_tree中查找节点
+void rb_tree_find(struct rb_tree * tree, struct rb_tree * find_node){
     
 }
--- lib/rb_tree.h
+++ lib/rb_tree.h
@@ -8,7 +8,6 @@
  * （3）每个叶子节点（NIL）是黑色。[注意：这里叶子节点，是指为空(NIL或NULL)的叶子节点]
  * （4）如果一个节点是红色的，则它的子节点必须是黑色的。
  * （5）从一个节点到该节点的子孙节点的所有路径上包含相同数目的黑节点。
- * 
  */
 
 enum rb_color{
@@ -23,5 +22,14 @@ struct rb_tree{
 
 // 初始化
 void rb_tree_init(struct rb_tree * tree);
+
+// 插入节点
+void rb_tree_insert(struct rb_tree * tree, struct rb_tree * in_node);
+
+// 删除节点
+void rb_tree_remove(struct rb_tree * tree, struct rb_tree * out_node);
+
+// 查找节点
+void rb_tree_find(struct rb_tree * tree, struct rb_tree * find_node);
 
 #endif
